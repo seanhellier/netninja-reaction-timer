a1c33a0781f08bd531cd79f5b75465bdde5c39f5
Sean Leif Hellier <sean@newtropolis.com>
2021-06-25 10:03:11 -0400

    lesson 36

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">reaction time is {{ score }} ms</p>
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
+// when the game ends, show the results component
+// output the score inside the results component
 import Block from './components/Block'
+import Results from './components/Results'
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
@@ -23,7 +26,7 @@ export default {
       // set time amount (ms)
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-         this.showResults = false
+      this.showResults = false
     },
     endGame(reactionTime) {
       this.score = reactionTime
---
+++ src/components/Results.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+  props: ['score']
+}
+</script>
