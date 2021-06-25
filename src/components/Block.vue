a44b400692b0332211020be5bf438211dc233db6
Sean Leif Hellier <sean@newtropolis.com>
2021-06-25 08:23:24 -0400

    lesson 34

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">click me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
 </template>
 
 <script>
@@ -7,17 +7,32 @@ export default {
     props: [ "delay" ],
     data() {
         return {
-            showBlock: false
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
         }
     },
     mounted() {
         console.log("component mounted")
         setTimeout(() => {
             this.showBlock = true
-        },
+            this.startTimer()
+         },
         this.delay
         )
     },
+    methods: {
+        startTimer () {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+        }
+    }
 }
 </script>
 
