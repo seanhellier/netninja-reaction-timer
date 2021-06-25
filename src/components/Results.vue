e94d905b9e33de7a85614f400b171676d62806e2
Sean Leif Hellier <sean@newtropolis.com>
2021-06-25 10:41:46 -0400

    lesson 37

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,34 @@
 
 <template>
   <p>Reaction time - {{ score }} ms</p>
+  <p class="rank "> {{ rank }}</p>
 </template>
 
 <script>
 export default {
-  props: ['score']
-}
+  props: ['score'],
+  data() {
+      return {
+          rank: null
+      }
+  },
+  mounted() {
+      if (this.score < 250 ){
+          this.rank = "ninja fingers"
+      } else if (this.score < 400) {
+          this.rank = "rapid reflexes"
+      } else {
+          this.rank = "snail's pace"
+      }
+  }  
+  }
 </script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: aqua;
+    font-weight: bold;
+}
+
+</style>
